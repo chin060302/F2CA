d1e9ddfa9039e4a5db31eeec563a71f8f5c61c87
ACS109144 <93269592+chin060302@users.noreply.github.com>
2023-06-26 12:52:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+import time
+
+options=Options()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
+driver.get('https://accounts.google.com/')
+
+
+try:
+    username_input = driver.find_element(By.CSS_SELECTOR, 'input[type="text"], input[type="email"], input[name="username"], input[name="email"]')
+    username_input.send_keys('youremail')
+except NoSuchElementException as e:
+    print('找不到帳號輸入欄位')
+
+
+while True:
+    try:
+        password_input = driver.find_element(By.CSS_SELECTOR, 'input[type="password"], input[name="password"]')
+        password_input.send_keys('yourpassword')
+        break
+    except Exception as e: 
+        next_button = driver.find_element(By.ID, 'identifierNext')
+        next_button.click()
+        time.sleep(3)
+        continue
+
 
 =======
 from selenium import webdriver
@@ -18,4 +50,4 @@ password = browser.find_element(By.ID, "password")
 
 username.send_keys("username")
 password.send_keys("password")
->>>>>>> 2c43768e18edab4544ab0537d3cf7e36a493cd9b
+
