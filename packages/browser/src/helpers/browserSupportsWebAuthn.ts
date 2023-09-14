6db4f88721a9fe3e3d9996c58ef749544bef339a
acs109144 <acs109144@gm.ntcu.edu.tw>
2023-09-14 13:03:53 +0800

    123

--- packages/browser/src/helpers/browserSupportsWebAuthn.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Determine if the browser is capable of Webauthn
- */
-export function browserSupportsWebAuthn(): boolean {
-  return (
-    window?.PublicKeyCredential !== undefined && typeof window.PublicKeyCredential === 'function'
-  );
-}
--- packages/server/src/helpers/decodeCredentialPublicKey.ts
+++
@@ -1,6 +0,0 @@
-import { COSEPublicKey } from './cose';
-import { isoCBOR } from './iso';
-
-export function decodeCredentialPublicKey(publicKey: Uint8Array): COSEPublicKey {
-  return isoCBOR.decodeFirst<COSEPublicKey>(publicKey);
-}
