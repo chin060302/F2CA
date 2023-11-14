2f4cfd9cded69c181ae0503f9afc0414abcf716d
acs109144 <acs109144@gm.ntcu.edu.tw>
2023-11-14 15:27:58 +0800

    123

--- example/index.ts
+++ example/index.ts
@@ -114,9 +114,9 @@ let loggedInUserId = 'internalUserId';
 import mysql from 'mysql2';
 
 var con = mysql.createConnection({
-  host: "f2ca-database.cqu2x3hha8ky.ap-southeast-2.rds.amazonaws.com",
+  host: "db-1.cel5xmxxast7.ap-southeast-2.rds.amazonaws.com",
   user: "admin",
-  password: "f2capassword"
+  password: "F2CApassword"
 });
 
 con.connect(function(err) {
